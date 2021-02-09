cd54ce5dbdf20d313bd6578fb5e94224527b5d70
Stuart Kozola <stuart.kozola@gmail.com>
2021-02-09 15:59:22 -0500

    updates for tasks 4

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,20 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath="kozola/boston_ml_api"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run bostonml \
+    --generator=run-pod/v1 \
+    --image=$dockerpath \
+    --port=80 \
+    --labels app=bostonml
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward bostonml 8000:80
